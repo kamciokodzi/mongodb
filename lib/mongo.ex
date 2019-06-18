99e96a4ae6b963914ab733a8ba551ad1fcf75b2c
Kamil Kolodziej <kamil.kolodziej@appunite.com>
2019-06-18 15:25:16 +0200

    fix aggregate function cursors

--- lib/mongo.ex
+++ lib/mongo.ex
@@ -70,20 +70,14 @@ defmodule Mongo do
     ] |> filter_nils
 
     cursor? = pool.version >= 1 and Keyword.get(opts, :use_cursor, true)
-
-    query = if cursor? do
-      cursor = %{
-        batchSize: opts[:batch_size]
-      }
-      query ++ [cursor: filter_nils(cursor)]
-    end
-
     opts = Keyword.drop(opts, ~w(allow_disk_use max_time use_cursor)a)
 
     if cursor? do
+      query = query ++ [cursor: filter_nils(%{batchSize: opts[:batch_size]})]
       aggregation_cursor(pool, "$cmd", query, nil, opts)
     else
-      singly_cursor(pool, "$cmd", query, nil, opts)
+      query = query ++ [cursor: %{}]
+      aggregation_cursor(pool, "$cmd", query, nil, opts)
     end
   end
 
